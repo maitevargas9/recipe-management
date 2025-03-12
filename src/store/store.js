2264a1d5aee5277084ab1b4979456fd0748a30b8
maitevargas9 <132816437+maitevargas9@users.noreply.github.com>
2025-03-12 16:02:53 +0100

    Expansion of the store to add, change and delete recipes.

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,36 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
+const loadRecipes = () => JSON.parse(localStorage.getItem("recipes")) || [];
+const saveRecipes = (recipes) =>
+  localStorage.setItem("recipes", JSON.stringify(recipes));
+
+const recipesSlice = createSlice({
+  name: "recipes",
+  initialState: loadRecipes(),
+  reducers: {
+    addRecipe: (state, action) => {
+      state.push(action.payload);
+      saveRecipes(state);
+    },
+    updateRecipe: (state, action) => {
+      const index = state.findIndex(
+        (recipe) => recipe.id === action.payload.id
+      );
+      if (index !== -1) {
+        state[index] = action.payload;
+        saveRecipes(state);
+      }
+    },
+    deleteRecipe: (state, action) => {
+      const updatedState = state.filter(
+        (recipe) => recipe.id !== action.payload
+      );
+      saveRecipes(updatedState);
+      return updatedState;
+    }
+  }
+});
+
 const favoritesSlice = createSlice({
   name: "favorites",
   initialState: JSON.parse(localStorage.getItem("favorites")) || [],
@@ -18,10 +49,12 @@ const favoritesSlice = createSlice({
   }
 });
 
+export const { addRecipe, updateRecipe, deleteRecipe } = recipesSlice.actions;
 export const { addFavorite, removeFavorite } = favoritesSlice.actions;
 
 const store = configureStore({
   reducer: {
+    recipes: recipesSlice.reducer,
     favorites: favoritesSlice.reducer
   }
 });
